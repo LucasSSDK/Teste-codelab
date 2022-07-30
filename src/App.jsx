6906687f5ea2492fbbdf0470e79f95af2b17c9a7
LucasSSDK <l_ucass_oares@outlook.com>
2022-07-30 16:44:23 -0300

    Hello World

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return(
+    <>
+    <HelloWorld/>
+    <HelloWorld/>
+    <HelloWorld/>
+    <HelloWorld/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import "./HelloWorld.css";
+
+function HelloWorld() {
+    return(
+        <div>
+            <h1> Hello World!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ipsa.
+            </p>
+        </div>
+    );
+}
+
+export default HelloWorld;
